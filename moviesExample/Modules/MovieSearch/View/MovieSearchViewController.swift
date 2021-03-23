a403e5fed2f616a523c12ed4a8bd2a332fb2f384
Diego Ferrari <dferrari@bcp.com.pe>
2021-03-23 17:39:43 -0500

    add spinner loading search

--- moviesExample/Modules/MovieSearch/View/MovieSearchViewController.swift
+++ moviesExample/Modules/MovieSearch/View/MovieSearchViewController.swift
@@ -14,11 +14,60 @@ class MovieSearchViewController: UIViewController {
     @IBOutlet weak var moviesTableView: UITableView!
     var presenter: MovieSearchViewPresenterProtocol?
     var movies: [Movie] = []
+    
+    /// View which contains the loading text and the spinner
+    let loadingView = UIView()
+
+    /// Spinner shown during load the TableView
+    let spinner = UIActivityIndicatorView()
+
+    /// Text shown during load the TableView
+    let loadingLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        presenter?.searchInMovies(search: "a", page: 1)
+//        presenter?.searchInMovies(search: "a", page: 1)
+        moviesSearchBar.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    // Set the activity indicator into the main view
+    private func setLoadingScreen() {
+
+        // Sets the view which contains the loading text and the spinner
+        let width: CGFloat = 120
+        let height: CGFloat = 30
+        let x = (moviesTableView.frame.width / 2) - (width / 2)
+        let y = (moviesTableView.frame.height / 2) - (height / 2) - (navigationController?.navigationBar.frame.height)!
+        loadingView.frame = CGRect(x: x, y: y, width: width, height: height)
+
+        // Sets loading text
+        loadingLabel.textColor = .gray
+        loadingLabel.textAlignment = .center
+        loadingLabel.text = "Cargando..."
+        loadingLabel.frame = CGRect(x: 0, y: 0, width: 140, height: 30)
+
+        // Sets spinner
+        spinner.style = .gray
+        spinner.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        spinner.startAnimating()
+
+        // Adds text and spinner to the view
+        loadingView.addSubview(spinner)
+        loadingView.addSubview(loadingLabel)
+
+        moviesTableView.addSubview(loadingView)
+
+    }
+
+    // Remove the activity indicator from the main view
+    private func removeLoadingScreen() {
+
+        // Hides and stops the text and the spinner
+        spinner.stopAnimating()
+        spinner.isHidden = true
+        loadingLabel.isHidden = true
+
     }
     
 
@@ -38,14 +87,19 @@ extension MovieSearchViewController: UITableViewDataSource, UITableViewDelegate{
 }
 
 extension MovieSearchViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        setLoadingScreen()
+        presenter?.searchInMovies(search: searchBar.text!, page: 1)
+    }
 }
 
 extension MovieSearchViewController: MovieSearchViewProtocol{
     func failInSearch() {
-        
+        removeLoadingScreen()
     }
     
     func showMoviesInSearch(movies: [Movie]) {
+        removeLoadingScreen()
         self.movies = movies
         moviesTableView.reloadData()
     }
